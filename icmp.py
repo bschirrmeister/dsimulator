3811cd645e99cc3de7ad7aa0a078e63eb75bc0a1
tmatias <tmatias@fa631a15-ca6c-7cfd-b61d-b47331e09e27>
2010-04-27 06:51:21 +0000

    force cheksum recalculation over ICMP and IP packets

--- icmp.py
+++ icmp.py
@@ -9,17 +9,30 @@ class ICMPState(State):
         message.mac = self.context.mac
         super(ICMPState, self).signal(message, signalQueue)
 
-class ICMP(ICMPState):
+class ICMPlayer(ICMPState):
     def on_icmp_echoRequest(self, message):
-        ICMPPacket=message.payload
-        # response with echo request
-        ICMPPacket['ICMP'].type=ECHO_REPLY
-        ICMPPacket['Ethernet'].dst=ICMPPacket['Ethernet'].src
-        ICMPPacket['IP'].dst,ICMPPacket['IP'].src=ICMPPacket['IP'].src,ICMPPacket['IP'].dst
-        # src should be set by ethernet layer....
-        ICMPPacket['Ethernet'].src=''
+        icmpLogger.debug("ICMP_ECHOREQUEST arrived for %s", message.payload['Ethernet'].dst)
+        
+        ICMPPacket = message.payload
+        NICMPPacket = ICMPPacket 
 
-        m = Message("send_packet", mac=self.context.mac, payload=ICMPPacket)
+        # for IP and ICMP chksum recalculation
+        del(NICMPPacket['IP'].chksum)
+        del(NICMPPacket['IP'].len)
+        del(NICMPPacket['ICMP'].chksum)
+
+        # response with echo request
+        NICMPPacket['ICMP'].type=ECHO_REPLY
+        NICMPPacket['Ethernet'].dst=ICMPPacket['Ethernet'].src
+        # src should be set by ethernet layer....
+        NICMPPacket['Ethernet'].src=''
+        NICMPPacket['IP'].dst,NICMPPacket['IP'].src=ICMPPacket['IP'].src,ICMPPacket['IP'].dst
+        NICMPPacket['IP'].flags=0
+        #NICMPPacket['IP'].ttl=128
+        NICMPPacket['IP'].id=random.randint(1000,5000)
+        #NICMPPacket['ICMP'].load = ICMPPacket['ICMP'].load
+
+        m = Message("send_packet", mac=self.context.mac, payload=NICMPPacket)
         self.signal(m)
 
         return self
